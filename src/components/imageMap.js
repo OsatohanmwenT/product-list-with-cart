799a9e1982493cd63f131c33b256c44b0ff313f7
OsatohanmwenT <143819001+OsatohanmwenT@users.noreply.github.com>
2024-07-25 20:18:44 +0100

    commit

--- src/components/imageMap.js
+++ src/components/imageMap.js
@@ -1,48 +1,48 @@
 // src/assets/imageMap.js
-import imageWaffleThumbnail from './images/image-waffle-thumbnail.jpg';
-import imageWaffleMobile from './images/image-waffle-mobile.jpg';
-import imageWaffleTablet from './images/image-waffle-tablet.jpg';
-import imageWaffleDesktop from './images/image-waffle-desktop.jpg';
+import imageWaffleThumbnail from '/assets/images/image-waffle-thumbnail.jpg';
+import imageWaffleMobile from '/assets/images/image-waffle-mobile.jpg';
+import imageWaffleTablet from '/assets/images/image-waffle-tablet.jpg';
+import imageWaffleDesktop from '/assets/images/image-waffle-desktop.jpg';
 
-import imageCremeBruleeThumbnail from './images/image-creme-brulee-thumbnail.jpg';
-import imageCremeBruleeMobile from './images/image-creme-brulee-mobile.jpg';
-import imageCremeBruleeTablet from './images/image-creme-brulee-tablet.jpg';
-import imageCremeBruleeDesktop from './images/image-creme-brulee-desktop.jpg';
+import imageCremeBruleeThumbnail from '/assets/images/image-creme-brulee-thumbnail.jpg';
+import imageCremeBruleeMobile from '/assets/images/image-creme-brulee-mobile.jpg';
+import imageCremeBruleeTablet from '/assets/images/image-creme-brulee-tablet.jpg';
+import imageCremeBruleeDesktop from '/assets/images/image-creme-brulee-desktop.jpg';
 
-import imageMacaronThumbnail from './images/image-macaron-thumbnail.jpg';
-import imageMacaronMobile from './images/image-macaron-mobile.jpg';
-import imageMacaronTablet from './images/image-macaron-tablet.jpg';
-import imageMacaronDesktop from './images/image-macaron-desktop.jpg';
+import imageMacaronThumbnail from '/assets/images/image-macaron-thumbnail.jpg';
+import imageMacaronMobile from '/assets/images/image-macaron-mobile.jpg';
+import imageMacaronTablet from '/assets/images/image-macaron-tablet.jpg';
+import imageMacaronDesktop from '/assets/images/image-macaron-desktop.jpg';
 
-import imageTiramisuThumbnail from './images/image-tiramisu-thumbnail.jpg';
-import imageTiramisuMobile from './images/image-tiramisu-mobile.jpg';
-import imageTiramisuTablet from './images/image-tiramisu-tablet.jpg';
-import imageTiramisuDesktop from './images/image-tiramisu-desktop.jpg';
+import imageTiramisuThumbnail from '/assets/images/image-tiramisu-thumbnail.jpg';
+import imageTiramisuMobile from '/assets/images/image-tiramisu-mobile.jpg';
+import imageTiramisuTablet from '/assets/images/image-tiramisu-tablet.jpg';
+import imageTiramisuDesktop from '/assets/images/image-tiramisu-desktop.jpg';
 
-import imageBaklavaThumbnail from './images/image-baklava-thumbnail.jpg';
-import imageBaklavaMobile from './images/image-baklava-mobile.jpg';
-import imageBaklavaTablet from './images/image-baklava-tablet.jpg';
-import imageBaklavaDesktop from './images/image-baklava-desktop.jpg';
+import imageBaklavaThumbnail from '/assets/images/image-baklava-thumbnail.jpg';
+import imageBaklavaMobile from '/assets/images/image-baklava-mobile.jpg';
+import imageBaklavaTablet from '/assets/images/image-baklava-tablet.jpg';
+import imageBaklavaDesktop from '/assets/images/image-baklava-desktop.jpg';
 
-import imageMeringueThumbnail from './images/image-meringue-thumbnail.jpg';
-import imageMeringueMobile from './images/image-meringue-mobile.jpg';
-import imageMeringueTablet from './images/image-meringue-tablet.jpg';
-import imageMeringueDesktop from './images/image-meringue-desktop.jpg';
+import imageMeringueThumbnail from '/assets/images/image-meringue-thumbnail.jpg';
+import imageMeringueMobile from '/assets/images/image-meringue-mobile.jpg';
+import imageMeringueTablet from '/assets/images/image-meringue-tablet.jpg';
+import imageMeringueDesktop from '/assets/images/image-meringue-desktop.jpg';
 
-import imageCakeThumbnail from './images/image-cake-thumbnail.jpg';
-import imageCakeMobile from './images/image-cake-mobile.jpg';
-import imageCakeTablet from './images/image-cake-tablet.jpg';
-import imageCakeDesktop from './images/image-cake-desktop.jpg';
+import imageCakeThumbnail from '/assets/images/image-cake-thumbnail.jpg';
+import imageCakeMobile from '/assets/images/image-cake-mobile.jpg';
+import imageCakeTablet from '/assets/images/image-cake-tablet.jpg';
+import imageCakeDesktop from '/assets/images/image-cake-desktop.jpg';
 
-import imageBrownieThumbnail from './images/image-brownie-thumbnail.jpg';
-import imageBrownieMobile from './images/image-brownie-mobile.jpg';
-import imageBrownieTablet from './images/image-brownie-tablet.jpg';
-import imageBrownieDesktop from './images/image-brownie-desktop.jpg';
+import imageBrownieThumbnail from '/assets/images/image-brownie-thumbnail.jpg';
+import imageBrownieMobile from '/assets/images/image-brownie-mobile.jpg';
+import imageBrownieTablet from '/assets/images/image-brownie-tablet.jpg';
+import imageBrownieDesktop from '/assets/images/image-brownie-desktop.jpg';
 
-import imagePannaCottaThumbnail from './images/image-panna-cotta-thumbnail.jpg';
-import imagePannaCottaMobile from './images/image-panna-cotta-mobile.jpg';
-import imagePannaCottaTablet from './images/image-panna-cotta-tablet.jpg';
-import imagePannaCottaDesktop from './images/image-panna-cotta-desktop.jpg';
+import imagePannaCottaThumbnail from '/assets/images/image-panna-cotta-thumbnail.jpg';
+import imagePannaCottaMobile from '/assets/images/image-panna-cotta-mobile.jpg';
+import imagePannaCottaTablet from '/assets/images/image-panna-cotta-tablet.jpg';
+import imagePannaCottaDesktop from '/assets/images/image-panna-cotta-desktop.jpg';
 
 const imageMap = {
   "image-waffle-thumbnail": imageWaffleThumbnail,
