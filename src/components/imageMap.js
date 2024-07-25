abf08434038568c7f1fc93b5d9867acfe494bfa5
OsatohanmwenT <143819001+OsatohanmwenT@users.noreply.github.com>
2024-07-25 20:05:59 +0100

    Create imageMap.js

---
+++ src/components/imageMap.js
@@ -0,0 +1,94 @@
+// src/assets/imageMap.js
+import imageWaffleThumbnail from './images/image-waffle-thumbnail.jpg';
+import imageWaffleMobile from './images/image-waffle-mobile.jpg';
+import imageWaffleTablet from './images/image-waffle-tablet.jpg';
+import imageWaffleDesktop from './images/image-waffle-desktop.jpg';
+
+import imageCremeBruleeThumbnail from './images/image-creme-brulee-thumbnail.jpg';
+import imageCremeBruleeMobile from './images/image-creme-brulee-mobile.jpg';
+import imageCremeBruleeTablet from './images/image-creme-brulee-tablet.jpg';
+import imageCremeBruleeDesktop from './images/image-creme-brulee-desktop.jpg';
+
+import imageMacaronThumbnail from './images/image-macaron-thumbnail.jpg';
+import imageMacaronMobile from './images/image-macaron-mobile.jpg';
+import imageMacaronTablet from './images/image-macaron-tablet.jpg';
+import imageMacaronDesktop from './images/image-macaron-desktop.jpg';
+
+import imageTiramisuThumbnail from './images/image-tiramisu-thumbnail.jpg';
+import imageTiramisuMobile from './images/image-tiramisu-mobile.jpg';
+import imageTiramisuTablet from './images/image-tiramisu-tablet.jpg';
+import imageTiramisuDesktop from './images/image-tiramisu-desktop.jpg';
+
+import imageBaklavaThumbnail from './images/image-baklava-thumbnail.jpg';
+import imageBaklavaMobile from './images/image-baklava-mobile.jpg';
+import imageBaklavaTablet from './images/image-baklava-tablet.jpg';
+import imageBaklavaDesktop from './images/image-baklava-desktop.jpg';
+
+import imageMeringueThumbnail from './images/image-meringue-thumbnail.jpg';
+import imageMeringueMobile from './images/image-meringue-mobile.jpg';
+import imageMeringueTablet from './images/image-meringue-tablet.jpg';
+import imageMeringueDesktop from './images/image-meringue-desktop.jpg';
+
+import imageCakeThumbnail from './images/image-cake-thumbnail.jpg';
+import imageCakeMobile from './images/image-cake-mobile.jpg';
+import imageCakeTablet from './images/image-cake-tablet.jpg';
+import imageCakeDesktop from './images/image-cake-desktop.jpg';
+
+import imageBrownieThumbnail from './images/image-brownie-thumbnail.jpg';
+import imageBrownieMobile from './images/image-brownie-mobile.jpg';
+import imageBrownieTablet from './images/image-brownie-tablet.jpg';
+import imageBrownieDesktop from './images/image-brownie-desktop.jpg';
+
+import imagePannaCottaThumbnail from './images/image-panna-cotta-thumbnail.jpg';
+import imagePannaCottaMobile from './images/image-panna-cotta-mobile.jpg';
+import imagePannaCottaTablet from './images/image-panna-cotta-tablet.jpg';
+import imagePannaCottaDesktop from './images/image-panna-cotta-desktop.jpg';
+
+const imageMap = {
+  "image-waffle-thumbnail": imageWaffleThumbnail,
+  "image-waffle-mobile": imageWaffleMobile,
+  "image-waffle-tablet": imageWaffleTablet,
+  "image-waffle-desktop": imageWaffleDesktop,
+
+  "image-creme-brulee-thumbnail": imageCremeBruleeThumbnail,
+  "image-creme-brulee-mobile": imageCremeBruleeMobile,
+  "image-creme-brulee-tablet": imageCremeBruleeTablet,
+  "image-creme-brulee-desktop": imageCremeBruleeDesktop,
+
+  "image-macaron-thumbnail": imageMacaronThumbnail,
+  "image-macaron-mobile": imageMacaronMobile,
+  "image-macaron-tablet": imageMacaronTablet,
+  "image-macaron-desktop": imageMacaronDesktop,
+
+  "image-tiramisu-thumbnail": imageTiramisuThumbnail,
+  "image-tiramisu-mobile": imageTiramisuMobile,
+  "image-tiramisu-tablet": imageTiramisuTablet,
+  "image-tiramisu-desktop": imageTiramisuDesktop,
+
+  "image-baklava-thumbnail": imageBaklavaThumbnail,
+  "image-baklava-mobile": imageBaklavaMobile,
+  "image-baklava-tablet": imageBaklavaTablet,
+  "image-baklava-desktop": imageBaklavaDesktop,
+
+  "image-meringue-thumbnail": imageMeringueThumbnail,
+  "image-meringue-mobile": imageMeringueMobile,
+  "image-meringue-tablet": imageMeringueTablet,
+  "image-meringue-desktop": imageMeringueDesktop,
+
+  "image-cake-thumbnail": imageCakeThumbnail,
+  "image-cake-mobile": imageCakeMobile,
+  "image-cake-tablet": imageCakeTablet,
+  "image-cake-desktop": imageCakeDesktop,
+
+  "image-brownie-thumbnail": imageBrownieThumbnail,
+  "image-brownie-mobile": imageBrownieMobile,
+  "image-brownie-tablet": imageBrownieTablet,
+  "image-brownie-desktop": imageBrownieDesktop,
+
+  "image-panna-cotta-thumbnail": imagePannaCottaThumbnail,
+  "image-panna-cotta-mobile": imagePannaCottaMobile,
+  "image-panna-cotta-tablet": imagePannaCottaTablet,
+  "image-panna-cotta-desktop": imagePannaCottaDesktop,
+};
+
+export default imageMap;
